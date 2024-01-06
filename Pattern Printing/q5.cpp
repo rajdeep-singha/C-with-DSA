3cb43ee9c9098f37bfd6a6f416d86dd44879d1da
Rajdeep Singha <137903887+RajuC0der@users.noreply.github.com>
2024-01-06 23:48:33 +0530

    Day3

--- Pattern Printing/q5.cpp
+++ Pattern Printing/q5.cpp
@@ -8,7 +8,7 @@ int main(){
             cout<<j;
         }
         for(int j=1;j<=(i);j++){
-            cout<<j;
+           cout<<j;
         }
         cout<<endl;
     }
---
+++ Pattern Printing/q7.cpp
@@ -0,0 +1,22 @@
+// main concept of pattern printing is relate the number of rows/columns with the variety of pattern we want to get
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=(n-1);j++){
+        cout<<" ";
+    }
+    for(int j=1;j<=i;j++){
+        cout<<j;
+    }
+    for(int j=(i-1);j>=1;j--){
+        cout<<j;
+    }
+    cout<<endl;
+}
+
+return 0;
+}
