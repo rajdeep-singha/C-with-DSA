c45297bb34bc97583f96dc866be3170e9ae64ae4
Rajdeep Singha <137903887+RajuC0der@users.noreply.github.com>
2024-01-08 22:48:40 +0530

    Day5

--- Pattern Printing/q4.cpp
+++ Pattern Printing/q4.cpp
@@ -7,8 +7,8 @@ int main(){
      for(int j=1;j<=(n-i);j++){
         cout<<" ";
      }  
-     for(int j=1;j<=(2*i-1);j++){
-        cout<<"*";
+    for(int j=1;j<=(2*i-1);j++){
+       cout<<"*";
      }
      cout<<endl;
     }
