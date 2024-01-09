0a410958374c517462993df1efeb778ce860dcbb
Rajdeep Singha <137903887+RajuC0der@users.noreply.github.com>
2024-01-09 23:18:08 +0530

    just with chai

---
+++ Array/arraywithchai1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class Array
+{
+    private: 
+    int capacity;
+    int lastIndex;
+    int *ptr;
+    public:
+      void createArray(int);
+      void insertInArray(int,int);
+};
+
+void Array::createArray(int cap)
+{capacity=cap;
+lastIndex=-1;
+int *ptr=new int[cap];
+}
+
+void Array::insertInArray(int index,int data)
+
+
+int main()
+{
+    int marks[100];
+ marks[2]=data
+    return 0;
+}
